b53b63249752e6039be5250f5a4433894d9a9915
Aleks_work <alemarkhel@yandex.ru>
2023-09-29 16:56:37 +0300

    cycles

--- operators.js
+++ operators.js
@@ -13,3 +13,13 @@ console.log(NaN || null || !3 || undefined || 5);//5
 console.log(NaN || (null && !3 && undefined) || 5);//5
 
 console.log((5 === 5 && 3 > 1) || 5);//true
+
+const hamburger = 3;
+const fries = 3;
+const cola = 0;
+const nuggets = 2;
+
+if ((hamburger === 3 && cola) || (fries === 3 && nuggets)) {
+	console.log("Done!");
+}
+//Done
