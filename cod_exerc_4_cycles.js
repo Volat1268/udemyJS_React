6eb05697df5d42259df17ab8b0637eff19f191c0
aleksandr_markhel <volatpy@gmail.com>
2023-10-03 23:57:26 +0300

    cod_extrc_5

--- cod_exerc_4_cycles.js
+++ cod_exerc_4_cycles.js
@@ -16,12 +16,12 @@ function firstTask() {
 
 // Место для второй задачи
 function secondTask() {
-    const data = [5, 10, 'Shopping', 20, 'Homework'];
+    const data = [5, 10, "Shopping", 20, "Homework"];
 	const dataOne = [];
     // Пишем решение вот тут
     for (let i = 0; i < data.length; i++) {
 		if (data[i] == Number) {
-			console.log()
+			console.log();
 		} 
 	}
     
@@ -32,7 +32,7 @@ function secondTask() {
 // Место для третьей задачи
 function thirdTask() {
     // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
-    const data = [5, 10, 'Shopping', 20, 'Homework'];
+    const data = [5, 10, "Shopping", 20, "Homework"];
     const result = [];
 
     // Пишем решение вот тут
@@ -44,15 +44,15 @@ function thirdTask() {
 
 
 
-const data = [5, 10, 'Shopping', 20, 'Homework']
+const data = [5, 10, "Shopping", 20, "Homework"];
 for (let i = 0; i < data.length; i++) {
 	if (typeof(data[i]) == "number") {
 		data[i] *= 2;
 	} else if (typeof(data[i]) == "string") {
-		data[i] += "-done"
+		data[i] += "-done";
 	}
 	}
-console.log(data)
+console.log(data);
 
 
     
---
+++ cod_exerc_5_cycles.js
@@ -0,0 +1,8 @@
+const height = 5;
+let line = "";
+for (let i = 1; i <= height; i++) {
+	line += "*";
+	console.log(line);
+}
+
+
