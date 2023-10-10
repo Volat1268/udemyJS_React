f34772302455d1fc0db57af71f1966897fa3463c
Aleks_work <alemarkhel@yandex.ru>
2023-10-10 15:37:01 +0300

    practice part3 finish

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
-/* eslint-disable indent */
+/* eslint-disable no-mixed-spaces-and-tabs */
 "use strict";
-
-let numberOfFilms;
-
-const personalMoviesDB = {
-	count: numberOfFilms,
-	movies: {},
-	actors: {},
-	genres: [],
-	privat: false,
-};
 
 // =============================== Part 1 =======================================
 /* Задание на урок:
@@ -36,6 +26,15 @@ const personalMoviesDB = {
 Проверить, чтобы все работало без ошибок в консоли */
 
 // ------------------------- Part 1. Solution ---------------------------------
+/*let numberOfFilms;
+
+const personalMoviesDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};*/
 
 // const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?","");
 
@@ -73,17 +72,34 @@ const personalMoviesDB = {
 4) Потренироваться и переписать цикл еще двумя способами*/
 
 // ------------------------- Part 2. Solution ---------------------------------
+/*
+let numberOfFilms;
+
+const personalMoviesDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false,
+};
+
 numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "");
 personalMoviesDB["count"] = numberOfFilms;
 
 for (let i = 0; i < 2; i++) {
-    const filmSeenName = prompt("Один из последних просмотренных фильмов?", ""),
-          filmSeenRate = prompt("На сколько оцените его?", "");
-    if (filmSeenName !== null && filmSeenRate !== null && filmSeenName !== "" && filmSeenRate !== "" && filmSeenName.length < 50) {
-        personalMoviesDB["movies"][filmSeenName] = filmSeenRate;
+	const filmSeenName = prompt("Один из последних просмотренных фильмов?", ""),
+		filmSeenRate = prompt("На сколько оцените его?", "");
+	if (
+		filmSeenName !== null &&
+		filmSeenRate !== null &&
+		filmSeenName !== "" &&
+		filmSeenRate !== "" &&
+		filmSeenName.length < 50
+	) {
+		personalMoviesDB["movies"][filmSeenName] = filmSeenRate;
 	} else {
-        i--;		
-    }
+		i--;
+	}
 }
 
 if (personalMoviesDB.count < 10) {
@@ -95,4 +111,98 @@ if (personalMoviesDB.count < 10) {
 }
 
 console.log(personalMoviesDB);
+*/
 
+// ================================= Part 3 ========================================
+
+/* Задание на урок:
+
+1) Первую часть задания повторить по уроку
+
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
+
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+
+P.S. Функции вызывать не обязательно*/
+
+
+// ------------------------- Part 3. Solution ---------------------------------
+let numberOfFilms;
+
+const personalMoviesDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false,
+};
+
+// __________solution task 1_______________
+function start() {
+	numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "");
+	while(numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms) ) {
+		numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "");			
+	}
+	personalMoviesDB["count"] = numberOfFilms;
+}
+
+
+// __________solution task 2______________
+function showMyDB (hidden) {
+	if (!hidden) {
+		console.log(personalMoviesDB);
+	}
+}
+
+
+// __________solution task 3______________
+function writeYourGenres() {
+	for (let i = 0; i < 3; i++) {
+		let yourGenre = prompt(`Ваш любимый жанр под номером ${i + 1}?`, "");
+		while(yourGenre == "" || yourGenre == null) {
+			yourGenre = prompt(`Ваш любимый жанр под номером ${i + 1}?`, "");
+		}
+		personalMoviesDB["genres"][i] = yourGenre;
+	}
+}
+
+function rememberMyFilms() {
+	for (let i = 0; i < 2; i++) {
+		const filmSeenName = prompt("Один из последних просмотренных фильмов?", ""),
+			filmSeenRate = prompt("На сколько оцените его?", "");
+		if (
+			filmSeenName !== null &&
+      filmSeenRate !== null &&
+      filmSeenName !== "" &&
+      filmSeenRate !== "" &&
+      filmSeenName.length < 50
+		) {
+			personalMoviesDB["movies"][filmSeenName] = filmSeenRate;
+		} else {
+			i--;
+		}
+	}
+}
+
+function detectPersonalLevel() {
+	if (personalMoviesDB.count < 10) {
+		alert("Просмотрено довольно мало фильмов");
+	} else if (personalMoviesDB.count >= 10 && personalMoviesDB.count <= 30) {
+		alert("Вы классический зритель");
+	} else {
+		alert("Вы киноман");
+	}
+}
+
+start();
+rememberMyFilms();
+writeYourGenres();
+detectPersonalLevel();
+showMyDB(personalMoviesDB.privat);
+
+
+
+// alert("I'm working");
