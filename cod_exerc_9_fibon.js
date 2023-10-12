bb574553211cc41bd9949afa6c6a84fbd9c1a9e5
Aleks_work <alemarkhel@yandex.ru>
2023-10-12 16:09:32 +0300

    cod_exerc_9 finish

--- cod_exerc_9_fibon.js
+++ cod_exerc_9_fibon.js
@@ -17,3 +17,26 @@ fib(1) => "0"
 
 fib(0) => ''"
 */
+const fib = function(count) {
+	let response = "0 1";
+	let firstNum = 0;
+	let secondNum  = 1;
+	if (typeof(count) != "number" || count === 0) {
+		return "_";
+	} else if (count === 1) {
+		return String(firstNum);
+	} else 	if (count === 2) {
+		return response;
+	} else {
+		for (let i = 2; i < count; i++) {
+			let thirdNum = firstNum + secondNum;
+			response += (" " + thirdNum);
+			firstNum = secondNum;
+			secondNum = thirdNum;
+		}
+	}
+	console.log(typeof(response));
+	return response;	
+};
+
+console.log(fib(10));
