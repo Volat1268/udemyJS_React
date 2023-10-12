871e9b0cb63a136328b448f4ca87cddea6207c4f
Aleks_work <alemarkhel@yandex.ru>
2023-10-12 10:25:39 +0300

    cod_exerc_8 finish

--- cod_exerc_8_func.js
+++ cod_exerc_8_func.js
@@ -45,3 +45,21 @@ findMaxNumber(1, 5, '6', '10');  =>  0
 У этой задачи есть очень много вариантов решения, в том числе и встроенное в JS. Подходит любое :)
 */
 //------------------- solution to the task 2----------------------------
+
+const findMaxNumber = function(a, b, c, d) {
+	const arr = [a, b, c, d];
+	let max = a;
+	for (let i = 0; i < 4; i++) {
+		if (typeof(arr[i]) !== "number") {
+			return 0;
+		} else {
+			if (arr[i] > max) {
+				max = arr[i];
+			}
+		}		
+	}
+	return max;
+	
+};
+
+console.log(findMaxNumber(6.6, 11, 234, 4));
