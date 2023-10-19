1c9e39e4b83b6308dfc755c04f528f9a58eee9eb
Aleks_work <alemarkhel@yandex.ru>
2023-10-19 08:31:09 +0300

    cod_exerc_10 finish

--- cod_exerc_10_obj.js
+++ cod_exerc_10_obj.js
@@ -9,6 +9,9 @@ const personalPlanPeter = {
 		},
 		exp: "1 month",
 	},
+	showAgeAndLangs: function(data) {
+		console.log(`Мне ${data.age} лет и я владею языками: ${data.skills.languages.join(" ").toUpperCase()}`);
+	}
 };
 
 /*
@@ -17,15 +20,12 @@ const personalPlanPeter = {
 showExperience(personalPlanPeter) => '1 month'
 P.S. желательно использовать деструктуризацию, но не обязательно
 */
-function showExperience(data) {
-	const {exp} = data.skills;
-	return exp;
-}
+// function showExperience(data) {
+// 	const {exp} = data.skills;
+// 	return exp;
+// }
 
-console.log(showExperience(personalPlanPeter));
-
-	
-
+// console.log(showExperience(personalPlanPeter));
 
 /*
 2) Напишите функцию showProgrammingLangs, которая будет принимать в себя объект со всеми данными и возвращать строку в нужном виде.
@@ -36,11 +36,23 @@ showProgrammingLangs(personalPlanPeter)  =>
 P.S. Для переноса строки используется \n в конце строки.
 */
 
+// function showProgrammingLangs(data) {
+// 	const {programmingLangs} = data.skills;
+// 	let response = "";
+// 	for (let key in programmingLangs) {
+// 		response += `Язык ${key} изучен на ${programmingLangs[key]}\n`;
+// 	}
+// 	return response;
+// }
+
+// console.log(showProgrammingLangs(personalPlanPeter));
+
 /*
 3) Создайте метод showAgeAndLangs внутри объекта personalPlanPeter. При его вызове метод будет принимать в себя объект и возвращать строку в нужном виде.
 Пример:
 personalPlanPeter.showAgeAndLangs(personalPlanPeter)
 => 'Мне 29 и я владею языками: RU ENG'
 Заметьте, что возраст и языки подставляются автоматически из объекта, а языки всегда в верхнем регистре (большими буквами). Если данные в объекте поменяются, то и сообщение тоже изменится.
-
 */
+
+personalPlanPeter.showAgeAndLangs(personalPlanPeter);
