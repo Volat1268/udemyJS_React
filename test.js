8f178c8612525fddbc7eb5789b627c41b89d41a7
Aleks_work <alemarkhel@yandex.ru>
2023-11-02 17:10:25 +0300

    start OOP

--- test.js
+++ test.js
@@ -1,13 +1,3 @@
-const arr1 = [1, 2, 3, 4];
-const arr2 = [11, 22, 33, 44];
-const allArr1 = [...arr1, ...arr2];
-const allArr2 = [].concat(arr1, arr2);
-const allArr3 = arr1.concat(arr2);
-arr1.push(...arr2);
+"use strict";
 
-
-console.log(allArr1);
-console.log(allArr2);
-console.log(allArr3);
-console.log(arr1);
 
