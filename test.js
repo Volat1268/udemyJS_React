9b8e95a4d7bed707441503ee45a73309a5ddd857
aleksandr_markhel <volatpy@gmail.com>
2023-11-02 23:18:09 +0300

    test2

--- test.js
+++ test.js
@@ -1,3 +1,19 @@
 "use strict";
 
+const soldier = {
+	arm: "canon",
+	country: "USA",
+	power: 34
+};
+
+const franc = {};
+franc.__proto__ = soldier;
+console.log(franc.country);
+
+const mikle = {};
+Object.setPrototypeOf(mikle, soldier);
+console.log(mikle.arm);
+
+const nicole = Object.create(soldier);
+console.log(nicole.power);
 
