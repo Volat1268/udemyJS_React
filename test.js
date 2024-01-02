d20414d56276cd40d78407e2dfe18535602053fd
aleksandr_markhel <volatpy@gmail.com>
2024-01-02 20:28:50 +0300

    linarsearch

--- test.js
+++ test.js
@@ -1,6 +1,19 @@
 "use strict";
 
 const a = [1, 2, 3, 4, 5];
-const s = a.splice(1, 2, 11, 12, 13);
-console.log(a);
-console.log(s);
+// const s = a.splice(1, 2, 11, 12, 13);
+// console.log(a);
+// console.log(s);
+
+function linearSearch(t, a) {
+	let n = a.length, i = 0;
+	// a[n] = t;
+	while (a[i] !== t) i++;
+	if (i < n) {
+		return i;
+	} else {
+		return -1;
+	}	
+}
+
+console.log(linearSearch(14, a));
