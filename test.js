02a77fb6862a987ca77f2d2c1bb628507032758c
aleksandr_markhel <volatpy@gmail.com>
2024-01-03 22:43:12 +0300

    cod exerc 10

--- test.js
+++ test.js
@@ -3,22 +3,36 @@ const personalPlanPeter = {
 	name: "Peter",
 	age: "29",
 	skills: {
-		languages: ["ru", "eng"],
+		languages: ["ru", "eng", "esp"],
 		programmingLangs: {
 			js: "20%",
 			php: "10%",
+			python: "30%"
 		},
 		exp: "1 month",
 	},
+	showAgeAndLangs: function(plan) {
+		console.log(`Мне ${plan.age} лет и я владею языками: ${plan.skills.languages.join(" ").toUpperCase()}`);
+	}
 };
 
 function showExperience(plan) {
-
+	const{exp} = plan.skills;
+	return exp;
 }
 
-function showProgrammingLangs(plan) {}
+function showProgrammingLangs(plan) {
+	const {programmingLangs} = plan.skills;
+	let responce = [];
+	for (let key in programmingLangs) {
+		responce.push(`Язык ${key} изучен на ${programmingLangs[key]}`);
+	}
+	return responce.join("\n");
+	
+}
+
+// console.log(showProgrammingLangs(personalPlanPeter));
+personalPlanPeter.showAgeAndLangs(personalPlanPeter);
 
 
-const user = {};
-[user.firstName, user.lastName] = ["Aleks", "Puprkin"];
-console.log(user);
+
