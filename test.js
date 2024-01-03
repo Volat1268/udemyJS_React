00abc104ecf5301cbf4c41f4d38c6f0b6001c9aa
Aleks_work <alemarkhel@yandex.ru>
2024-01-03 08:58:04 +0300

    destructurizacija

--- test.js
+++ test.js
@@ -1,19 +1,24 @@
 "use strict";
+const personalPlanPeter = {
+	name: "Peter",
+	age: "29",
+	skills: {
+		languages: ["ru", "eng"],
+		programmingLangs: {
+			js: "20%",
+			php: "10%",
+		},
+		exp: "1 month",
+	},
+};
 
-const a = [1, 2, 3, 4, 5];
-// const s = a.splice(1, 2, 11, 12, 13);
-// console.log(a);
-// console.log(s);
+function showExperience(plan) {
 
-function linearSearch(t, a) {
-	let n = a.length, i = 0;
-	// a[n] = t;
-	while (a[i] !== t) i++;
-	if (i < n) {
-		return i;
-	} else {
-		return -1;
-	}	
 }
 
-console.log(linearSearch(14, a));
+function showProgrammingLangs(plan) {}
+
+
+const user = {};
+[user.firstName, user.lastName] = ["Aleks", "Puprkin"];
+console.log(user);
