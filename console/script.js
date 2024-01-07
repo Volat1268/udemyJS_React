4e326c33636487e9c760af5c98cedaeeed848107
aleksandr_markhel <volatpy@gmail.com>
2024-01-07 19:30:06 +0300

    okrujenie

--- console/script.js
+++ console/script.js
@@ -1,24 +1,42 @@
+/* eslint-disable no-debugger */
 "use strict";
 
-function hello() {
-	console.log("Hello World");
+// function hello() {
+// 	console.log("Hello World");
+// }
+
+// hello();
+
+// function hi() {
+// 	console.log("Say hi!");
+// }
+
+// hi();
+
+// const arr = [1, 14, 4, 30, 54],
+// 			sorted = arr.sort(compareNum);
+
+
+// function compareNum(a, b) {
+// 	return a - b;
+// }
+
+// console.log(sorted);
+
+function createCounter() {
+	let counter = 0;
+
+	const myFunction = function() {
+		counter = counter + 1;
+		return counter;
+	};
+
+	return myFunction;
 }
 
-hello();
+const increment = createCounter();
+const c1 = increment();
+const c2 = increment();
+const c3 = increment();
 
-function hi() {
-	console.log("Say hi!");
-}
-
-hi();
-
-const arr = [1, 14, 4, 30, 54],
-			sorted = arr.sort(compareNum);
-
-
-function compareNum(a, b) {
-	return a - b;
-}
-
-console.log(sorted);
-
+console.log(c1, c2, c3);
