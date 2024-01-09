a58a949d0b631ef0ceedfc2eb511171f1e34872b
aleksandr_markhel <volatpy@gmail.com>
2024-01-09 22:56:47 +0300

    work with elements

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
 "use strict";
 
-const btn1 = document.querySelectorAll(".wrapper > div");
-console.log(btn1[0]);
+const box = document.getElementById("box");
+const btns = document.querySelectorAll("button");
+const circles = document.querySelectorAll(".circle");
+const hearts = document.querySelectorAll(".heart");
+const oneHeart = document.querySelector(".heart");
+
+console.log(btns);
+circles.forEach(element => {
+	element.style.cssText = "background-color: green; margin-top: 30px; width: 100px; height: 100px; margin-bottom: 30px";
+});
+
+
+
+
 			
 
